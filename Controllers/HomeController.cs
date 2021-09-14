52c39e82d156455eed01f86f9cd5e23299e06b47
Lake <contact@lakeys.net>
2021-09-14 00:42:30 -0400

    Move sample orders to HomeController, delete GridC

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Sample_ASP.NET_Site.Models;
 
 namespace Sample_ASP.NET_Site.Controllers
 {
@@ -14,5 +16,17 @@ namespace Sample_ASP.NET_Site.Controllers
 
             return View();
         }
-    }
+		public ActionResult Orders_Read([DataSourceRequest] DataSourceRequest request)
+		{
+			var result = Enumerable.Range(0, 50).Select(i => new Client
+			{
+				ID = i,
+				LastName = "Doe",
+				FirstName = "John",
+				Timestamp = new DateTime(1631570697845)
+			});
+
+			return Json(result.ToDataSourceResult(request));
+		}
+	}
 }
