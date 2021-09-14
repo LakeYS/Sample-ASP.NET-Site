e4f074bf3f4afeb75351e3566ba1f06b274daabb
Lake <contact@lakeys.net>
2021-09-14 10:21:11 -0400

    Create initial migration

---
+++ Migrations/202109140446495_InitialCreate.cs
@@ -0,0 +1,28 @@
+﻿namespace Sample_ASP.NET_Site.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clients",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        LastName = c.String(),
+                        FirstName = c.String(),
+                        Timestamp = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Clients");
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,36 @@
+﻿namespace Sample_ASP.NET_Site.Migrations
+{
+    using Sample_ASP.NET_Site.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Sample_ASP.NET_Site.Models.ManagerContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Sample_ASP.NET_Site.Models.ManagerContext";
+        }
+
+        protected override void Seed(Sample_ASP.NET_Site.Models.ManagerContext context)
+        {
+            var clients = new List<Client>
+            {
+                new Client
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Timestamp = DateTime.Parse("2021-09-14")
+                },
+                new Client
+                {
+                    FirstName = "Jane",
+                    LastName = "Doe",
+                    Timestamp = DateTime.Parse("2021-09-14")
+                }
+            };
+
+        }
+    }
+}
